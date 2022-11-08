7c78a7199114d930694ab100c0e766f82bd12c70
SERAFIN <fagnerserafim31@gmail.com>
2022-11-08 20:08:39 -0300

    Implementado READ ALL

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require('express')
 const app = express()
 
-//
+// Endpoints
+
 app.get('/', function (req, res) {
   res.send('Hello World!!!')
 })
 
 app.get("/oi", function (req, res){
     res.send("Olá cambada!")
+});
+
+// Lista de Informações
+
+const itens = ["A Caçadora", "Michael Myers", "O Espirito"];
+
+// Endpoint [GET] /killers READ ALL
+app.get("/killers", function (req, res) {
+  res.send(itens);
 });
 
 app.listen(3000, function () {
