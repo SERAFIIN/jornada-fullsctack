51bf3016f9f543613ef3ceb0238c12e38be1dafc
SERAFIN <fagnerserafim31@gmail.com>
2022-11-07 22:16:04 -0300

    Finalizando aula

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
 
+//
 app.get('/', function (req, res) {
   res.send('Hello World!!!')
 })
 
+app.get("/oi", function (req, res){
+    res.send("Olá cambada!")
+});
+
 app.listen(3000, function () {
-    console.log("Servidor rodando em LocalHost:3000")
+    console.log("Servidor rodando em http://localhost:3000")
 });
