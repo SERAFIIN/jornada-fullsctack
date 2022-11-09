55704375cb5df64d1ba5102ee90d6d2502079610
SERAFIN <fagnerserafim31@gmail.com>
2022-11-08 21:50:02 -0300

    Implementação do READ BY ID

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+// Sinalizando que estamos utilizando JSON no body
+
+app.use(express.json());
+
 // Endpoints
 
 app.get('/', function (req, res) {
@@ -18,6 +22,33 @@ const itens = ["A Caçadora", "Michael Myers", "O Espirito", "Ghostface"];
 // Endpoint [GET] /killers READ ALL
 app.get("/killers", function (req, res) {
   res.send(itens);
+});
+
+// Endpoint [POST] /killers CREATE (criar um item)
+
+app.post("/killers", function (req, res) {
+  console.log(req.body);
+
+  //Pegamos o nome enviado no body
+  const item = req.body.nome;
+
+  //Inserimos o valor recebido na lista
+  itens.push(item);
+
+  res.send("Item criado com sucesso!!")
+
+});
+
+// Endpoint [GET] /itens/:id - READ BY ID (Pelo ID)
+app.get("/killers/:id", function(req, res) {
+  //Pegamos o parametro de rota ID
+  const id = req.params.id - 1;
+
+  // Acessamos o item pelo indice
+  const item = itens[id];
+
+  // Exibimos o item encontrado
+  res.send(item);
 });
 
 app.listen(3000, function () {
