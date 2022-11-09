d63dc924ce608dce3b32920ec8d0eb2b65155e5c
SERAFIN <fagnerserafim31@gmail.com>
2022-11-08 22:07:24 -0300

    Implementado o PUT e o DELETE

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const itens = ["A Caçadora", "Michael Myers", "O Espirito", "Ghostface"];
 
 // Endpoint [GET] /killers READ ALL
 app.get("/killers", function (req, res) {
-  res.send(itens);
+  res.send(itens.filter(Boolean));
 });
 
 // Endpoint [POST] /killers CREATE (criar um item)
@@ -39,7 +39,7 @@ app.post("/killers", function (req, res) {
 
 });
 
-// Endpoint [GET] /itens/:id - READ BY ID (Pelo ID)
+// Endpoint [GET] /killers/:id - READ BY ID (Pelo ID)
 app.get("/killers/:id", function(req, res) {
   //Pegamos o parametro de rota ID
   const id = req.params.id - 1;
@@ -50,6 +50,33 @@ app.get("/killers/:id", function(req, res) {
   // Exibimos o item encontrado
   res.send(item);
 });
+
+// Endpoint [PUT] /killers:id - UPDATE BY ID
+app.put("/killers/:id", function (req, res) {
+  // Pegamos o parametro de rota ID
+  const id = req.params.id - 1;
+
+  // Pegamos o nome enviado no body
+  const item = req.body.nome;
+
+  // Atualizamos com o novo item, na posicao ID da lista de Killers
+  itens[id] = item;
+
+  res.send("Item atualizado com sucesso");
+});
+
+// Endpoint [DELETE] /killers/:id - DELETE BY ID
+app.delete("/killers/:id", function (req, res) {
+  // Pegamos o parametro de rota ID
+  const id =  req.params.id - 1;
+
+  // Remove o item da lista
+  delete itens[id];
+
+  // Exibimos uma mensagem de sucesso
+  res.send("Item removido com sucesso");
+});
+
 
 app.listen(3000, function () {
     console.log("Servidor rodando em http://localhost:3000")
